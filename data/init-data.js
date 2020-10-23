c950584af1f54811fd7a95b5bb106daadfeddd00
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-10-23 12:29:35 +1000

    Added some dummy/init data to load into DB

---
+++ data/init-data.js
@@ -0,0 +1,25 @@
+const tasks = [
+    {
+        name: "Read through all notes",
+        priority: 2,
+        dueDate: "2020-10-24"
+    },
+    {
+        name: "Do Laundry",
+        priority: 1
+    },
+    {
+        name: "Complete Week long project",
+        priority: 1,
+        dueDate: "2020-10-23"
+    },
+    {
+        name: "Grocery Shopping",
+        dueDate: "2020-10-23"
+    },
+    {
+        name: "Install mongo locally"
+    }
+]
+
+module.exports = tasks
